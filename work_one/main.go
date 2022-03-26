998a8a2354a2a962204c84a34ef6c07b1ba2ea2a
lics1216 <lics1216@163.com>
2022-03-26 22:48:26 +0800

    first task

--- work_one/main.go
+++ work_one/main.go
@@ -1,3 +1,9 @@
+/**
+### 第一次作业
+我们在数据库操作的时候，比如 dao 层中当遇到一个 sql.ErrNoRows 的时候，是否应该 Wrap 这个 error，抛给上层。为什么，应该怎么做请写出代码？
+
+我的答案：要往上抛，因为dao 自己处理不了就应该往上抛。可以调用errors.Wrap或者errors.New 方法往上抛，上层可以获得错误类型、错误根因、错误trace
+ */
 package main
 
 import (
@@ -11,19 +17,22 @@ import (
 	"time"
 )
 
+// 这是一个唱片的模型字段
 type Album struct {
 	ID     int64
-	Title  string
-	Artist string
-	Price  float32
+	Title  string // 名称
+	Artist string // 作者
+	Price  float32 // 价钱
 }
 
-// CreateOrder creates an order for an album and returns the new order ID.
+// 有一个创建订单的方法
+// 一个顾客想买 quantity 个唱片，唱片id是albumID，顾客id 是custID
 func CreateOrder(ctx context.Context, albumID, quantity, custID int) (orderID int64, err error) {
 	tx, err := db.BeginTx(ctx, nil)
 	defer tx.Rollback()
 
 	var enough bool
+	// 判断albumID 这种唱片库存有没有quantity 个，也有可能压根没有这种唱片
 	if err = tx.QueryRowContext(ctx, "SELECT (quantity >= ?) from album where id = ?",
 		quantity, albumID).Scan(&enough); err != nil {
 		if err == sql.ErrNoRows {
